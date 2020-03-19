e14634bcdb2bfbbf3efe8f0f71436c0fee0c5456
Federico <federico.baccaini@gmail.com>
2020-03-19 15:12:58 +0100

    Commit iniziale branch Federico

---
+++ src/main/java/com/kaleyra/db/orm/federico/Paziente.java
@@ -0,0 +1,22 @@
+package com.kaleyra.db.orm.federico;
+
+public class Paziente {
+    public int codiceComune;
+    private String provincia;
+    String cf;
+    String nome;
+    String cognome;
+    int age;
+    String comune_residenza;
+
+
+    Paziente(String name, String surname, String cf, int age, int codiceComune, String comune_residenza, String provincia) {
+        nome = name.toUpperCase();
+        cognome = surname.toUpperCase();
+        this.cf = cf.toUpperCase();
+        this.age = age;
+        this.comune_residenza = comune_residenza.toUpperCase();
+        this.provincia = provincia.toUpperCase();
+        this.codiceComune = codiceComune;
+    }
+}
